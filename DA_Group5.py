6bd0f38d647c419356daf9ee8832043669ad83c5
Aniyutharan <aniyutharanshanmugavel@gmail.com>
2021-08-29 01:12:07 +0800

    Checked and Amended for Zi Xuan

--- DA_Group5.py
+++ DA_Group5.py
@@ -24,26 +24,36 @@ rh = requests.get(url2, headers=headers)
 print(rh.text)
 # Source reference from "Lab 10b - Spoofing header in HTTP Get request"
 # ~Aniyutharan
-
-#6
+# ----------------------------------------------------------------------------------------------------------------------
+# T3
 import scrapy
-class NewSpider(scrapy.Spider):
-    name = "new spider"
+from scrapy.http.request import Request
+# scrapy runspider DA_Group5.py
+# T6 i
+class BrickSetSpider(scrapy.Spider):
+    name = "brick_spider"
     start_urls = ['https://brickset.com/sets/year-2002']
+    def start_requests(self):
+        headers = {'User-Agent':'Mozilla/5.0 (Linux; U; Android 2.3; en-us) AppleWebKit/999+ (KHTML, like Gecko) Safari/999.9'}
+        for url in self.start_urls:
+            yield Request(url, headers=headers)
     def parse(self, response):
         css_selector = 'img'
         for x in response.css(css_selector):
             newsel = '@src'
             yield {
-                'Image Link': x.xpath(newsel).extract_first()
+                'Image Link': x.xpath(newsel).extract_first(),
             }
-
-#7
-#To recurse next page
-    Page_selector = '.next a ::attr(href)'
-        next_page = response.css(Page_selector).extract.first()
+# T6 iii
+# scrapy runspider DA_Group5.py -o resultes.json -t json
+# T7
+        Page_selector = '.next a ::attr(href)'
+        next_page = response.css(Page_selector).extract_first()
         if next_page:
             yield scrapy.Request(
                 response.urljoin(next_page),
                 callback=self.parse
             )
+# Source reference from "Lab 10c - Using Scrapy webcrawler"
+# ~Zi Xuan
+# ----------------------------------------------------------------------------------------------------------------------
